7fc3ed81527bc84e990b09287765ef616a3d4a43
christian otkjær <christian.otkjaer@gmail.com>
2017-01-05 14:28:23 +0100

    Initial Commit

---
+++ Paging/Paging.h
@@ -0,0 +1,19 @@
+//
+//  Paging.h
+//  Paging
+//
+//  Created by Christian Otkjær on 05/01/17.
+//  Copyright © 2017 Silverback IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Paging.
+FOUNDATION_EXPORT double PagingVersionNumber;
+
+//! Project version string for Paging.
+FOUNDATION_EXPORT const unsigned char PagingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Paging/PublicHeader.h>
+
+
